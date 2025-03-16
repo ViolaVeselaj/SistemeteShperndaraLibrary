0b35115ce26950e2db3d9687279724cc97b18cbc
ViolaVeselaj <viole.veselaj@student.uni-pr.edu>
2025-03-16 13:19:19 +0100

    Initial commit

---
+++ src/main/java/com/example/sistemeteshperndara/SistemeTeShperndaraApplication.java
@@ -0,0 +1,13 @@
+package com.example.sistemeteshperndara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemeTeShperndaraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemeTeShperndaraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sistemeteshperndara/SistemeTeShperndaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sistemeteshperndara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemeTeShperndaraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
