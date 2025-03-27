87a031bcfbfcf699af9c7553e05071bf710a6aef
ViolaVeselaj <viole.veselaj@student.uni-pr.edu>
2025-03-27 21:09:35 +0100

    Create UserController.java
    Shtimi i  user controller

---
+++ backend/src/main/java/com/example/sistemeteshperndara/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.example.sistemeteshperndara.controller;
+
+import com.example.sistemeteshperndara.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping("/add")
+    public String addUser(@RequestBody User user) {
+        userService.saveUser(user);
+        return "Përdoruesi u shtua me sukses!";
+    }
+}
