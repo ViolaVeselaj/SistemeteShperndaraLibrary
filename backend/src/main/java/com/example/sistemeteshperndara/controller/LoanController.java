127fdbc490f554fea42e2b8e8abf9ed2ae1d2d77
ViolaVeselaj <viole.veselaj@student.uni-pr.edu>
2025-03-27 21:09:50 +0100

    Create LoanController.java
    Shtimi i loan controller

---
+++ backend/src/main/java/com/example/sistemeteshperndara/controller/LoanController.java
@@ -0,0 +1,26 @@
+package com.example.sistemeteshperndara.controller;
+
+import com.example.sistemeteshperndara.model.Loan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/loans")
+public class LoanController {
+
+    @Autowired
+    private LoanService loanService;
+
+    @GetMapping
+    public List<Loan> getAllLoans() {
+        return loanService.getAllLoans();
+    }
+
+    @PostMapping("/add")
+    public String addLoan(@RequestBody Loan loan) {
+        loanService.saveLoan(loan);
+        return "Huazimi u shtua me sukses!";
+    }
+}
