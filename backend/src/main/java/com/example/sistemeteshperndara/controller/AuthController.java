c4e570cdbc9d76894d552b098596eccc5540e8c2
ViolaVeselaj <viole.veselaj@student.uni-pr.edu>
2025-04-10 16:36:10 +0200

    LogIn backend

---
+++ backend/src/main/java/com/example/sistemeteshperndara/controller/AuthController.java
@@ -0,0 +1,36 @@
+package com.example.sistemeteshperndara.controller;
+
+import com.example.sistemeteshperndara.model.User;
+import com.example.sistemeteshperndara.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Optional;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody User loginRequest) {
+        Optional<User> userOptional = userRepository.findByEmail(loginRequest.getEmail());
+
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            if (passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())) {
+                return ResponseEntity.ok(user);
+            }
+        }
+
+        return ResponseEntity.status(401).body("Invalid email or password");
+    }
+}
