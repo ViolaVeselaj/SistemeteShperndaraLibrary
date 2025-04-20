8e3797f7eb67cec301ed5bd7edc68df9b778ab21
luragashi <lura.gashi@student.uni-pr.edu>
2025-04-20 23:20:33 +0200

    adminPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import HomePage from "./components/userHomePage";
+import AdminPage from "./components/AdminPage";
 
 const App = () => {
   return (
-    <HomePage />
+    <AdminPage />
   );
 };
 
