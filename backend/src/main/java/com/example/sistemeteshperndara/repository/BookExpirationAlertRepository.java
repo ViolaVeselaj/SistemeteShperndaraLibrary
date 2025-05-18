d6993acd774020641fb175fff11446b1782897ac
luragashi <lura.gashi@student.uni-pr.edu>
2025-05-18 15:10:46 +0200

    modeli BookExpirationAlert

---
+++ backend/src/main/java/com/example/sistemeteshperndara/repository/BookExpirationAlertRepository.java
@@ -0,0 +1,7 @@
+package com.example.sistemeteshperndara.repository;
+
+import com.example.sistemeteshperndara.model.BookExpirationAlert;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookExpirationAlertRepository extends JpaRepository<BookExpirationAlert, Long> {
+}
