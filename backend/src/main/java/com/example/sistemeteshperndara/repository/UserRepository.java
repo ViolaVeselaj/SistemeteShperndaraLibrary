93f967507764b963318308bc39a164124a0e73f7
dreninimanaj <nimanajdreni@gmail.com>
2025-05-19 12:17:40 +0200

    Get Users

--- backend/src/main/java/com/example/sistemeteshperndara/repository/UserRepository.java
+++ backend/src/main/java/com/example/sistemeteshperndara/repository/UserRepository.java
@@ -2,10 +2,15 @@ package com.example.sistemeteshperndara.repository;
 import com.example.sistemeteshperndara.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
     boolean existsByEmail(String email);
+
+
+    List<User> findAllByTenantId(Long tenantId);
+
 }
